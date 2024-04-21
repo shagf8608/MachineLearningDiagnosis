4ab6e1ac8fa5ef1b03b38bd09de77aa59e9ad5a5
shagf8608 <shagf8608@gmail.com>
2024-04-21 15:44:25 -0400

    empty

--- mldiagno/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Referdata, New_patient
-
-admin.site.register(Referdata)
-admin.site.register(New_patient)
--- mldiagno/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MldiagnoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mldiagno'
