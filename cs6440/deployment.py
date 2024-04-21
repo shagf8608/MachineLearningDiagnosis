c56cb5ddb81bdef4c7c4b95176ba4be5545b2a44
shagf8608 <shagf8608@gmail.com>
2024-04-21 19:17:56 -0400

    Update deployment.py

--- cs6440/deployment.py
+++ cs6440/deployment.py
@@ -8,7 +8,7 @@ DEBUG = False
 SECRET_KEY = os.environ["Secret"]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
