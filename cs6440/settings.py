d22cf2ab35314c91dcee2c38eafcd305d14c50c2
shagf8608 <shagf8608@gmail.com>
2024-04-21 23:07:42 -0400

    Update settings.py

--- cs6440/settings.py
+++ cs6440/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8c6^_9&_o@lohexs7pyippkf@bw83v7z@9fnmfy#8co=^xpm@y'
+SECRET_KEY = os.environ["Secret"]  #'django-insecure-8c6^_9&_o@lohexs7pyippkf@bw83v7z@9fnmfy#8co=^xpm@y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #True
 
 ALLOWED_HOSTS = ['*']
 
