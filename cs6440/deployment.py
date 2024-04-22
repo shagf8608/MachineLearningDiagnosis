f409e9c54f70f0c88e7f65e1e394e6c4151d996d
shagf8608 <shagf8608@gmail.com>
2024-04-22 19:15:06 -0400

    Update deployment.py

--- cs6440/deployment.py
+++ cs6440/deployment.py
@@ -23,6 +23,6 @@ connection_string = os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
